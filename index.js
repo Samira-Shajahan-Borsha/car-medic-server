8257bbf6fdff4cb08923459faf8164d1ea8515b6
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-04-02 08:00:22 +0600

    verify and decode jwt

--- index.js
+++ index.js
@@ -19,12 +19,29 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d0roctp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    // console.log(req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            res.status(401).send({ message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('carMedic').collection('services');
 
         const orderCollection = client.db('carMedic').collection('orders');
 
+        //jwt
         app.post('/jwt', (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACESS_TOKEN_SECRET, { expiresIn: '1h' });
@@ -50,15 +67,12 @@ async function run() {
         });
 
         //get all orders
-        app.get('/orders', async (req, res) => {
+        app.get('/orders', verifyJWT, async (req, res) => {
             // console.log(req.query.email);
-
             let query = {};
-
             if (req.query.email) {
                 query = { email: req.query.email };
             }
-
             const cursor = orderCollection.find(query);
             const orders = await cursor.toArray();
             res.send(orders);
